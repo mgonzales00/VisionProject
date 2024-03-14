3934a1c3c4334d771da4d6fcbe4c95cb4df402ba
Griffin2108 <gefreudenb@eagles.usi.edu>
2024-03-14 16:17:35 -0500

    Major changes with uploading image

--- WeedalyzerCode/VisionProject/VisionProject/ViewController.swift
+++ WeedalyzerCode/VisionProject/VisionProject/ViewController.swift
@@ -1,183 +1,108 @@
-import AVFoundation
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
     
-    var session: AVCaptureSession?
     
-    let output = AVCapturePhotoOutput()
     
-    let previewLayer = AVCaptureVideoPreviewLayer()
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var cameraPreview: UIImageView!
     
-    private var capturedImageView: UIImageView?
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var photoLibraryButton: UIButton!
     
-    private let shutterButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 75, height: 75))
-        button.layer.cornerRadius = 30
-        button.layer.borderWidth = 5
-        button.layer.borderColor = UIColor.white.cgColor
-        return button
-    }()
+    var imagePickerController = UIImagePickerController()
     
-    private let boxView: UIView = {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-        view.backgroundColor = UIColor.clear
-        view.layer.borderColor = UIColor.white.cgColor
-        view.layer.borderWidth = 3
-        view.layer.cornerRadius = 30
-        return view
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
-        view.layer.addSublayer(previewLayer)
-        view.addSubview(shutterButton)
-        view.addSubview(boxView) // Adding the box view
-        checkCameraPermissions()
-        
-        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
+        guard let documentsDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else{
+            return
+        }
+        print(documentsDirectory.path)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        previewLayer.frame = view.bounds
-        shutterButton.center = CGPoint(x: view.frame.size.width/2,
-                                       y: view.frame.size.height - 200)
-        // Set the frame for the boxView
-        
-        boxView.frame = CGRect(x: (view.frame.size.width - 300) / 2,
-                               y: (view.frame.size.height - 650) / 2,
-                               width: 300,
-                               height: 450)
+     
+    @IBAction func tappedCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
-    private func checkCameraPermissions(){
-        switch AVCaptureDevice.authorizationStatus(for: .video){
-        
-        case.notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                guard granted else{
-                    return
-                }
-                DispatchQueue.main.async{
-                    self.setUpCamera()
-                }
-            }
-        case .restricted:
-            break
-        case .denied:
-            break
-        case .authorized:
-            setUpCamera()
-        @unknown default:
-            break
+    @IBAction func tappedLibraryButton(_ sender: Any) {
+        self.imagePickerController.sourceType = .photoLibrary
+        self.present(self.imagePickerController, animated: true, completion: nil)
+    }
+    
+    func requestAuthorizationHandler(status: PHAuthorizationStatus){
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized {
+            print("Access granted to use Photo Library")
+        }
+        else{
+            print("We don't have access to your Photos.")
         }
     }
-    private func setUpCamera(){
-        let session = AVCaptureSession()
-        if let device = AVCaptureDevice.default(for: .video){
-            do {
-                let input = try AVCaptureDeviceInput(device: device)
-                if session.canAddInput(input) {
-                    session.addInput(input)
-                }
-                if session.canAddOutput(output) {
-                    session.addOutput(output)
-                    
-                }
-                
-                
-                previewLayer.videoGravity = .resizeAspectFill
-                previewLayer.session = session
-                
-                session.startRunning()
-                self.session = session
+    
+    
+        
+        
+    
+    
+}
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        if picker.sourceType == .camera {
+            cameraPreview.image = image
+            
+            
+        }
+        else if self.imagePickerController.sourceType == .photoLibrary{
+            cameraPreview.image = image
+        }
+    }
+    
+    func saveImageToImageFolder(image: UIImage){
+        guard let documentsDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else{
+            return
+        }
+        print(documentsDirectory.path)
+        let imageFolderURL = documentsDirectory.appendingPathComponent("Images")
+        
+        do{
+            try FileManager.default.createDirectory(at: imageFolderURL, withIntermediateDirectories: true,
+                attributes: nil)
+        }
+        catch{
+            print(error)
+            return
+        }
+        
+        let fileURL = imageFolderURL.appendingPathComponent("plant.jpeg")
+        
+        if let imageData = image.jpegData(compressionQuality: 1){
+            do{
+                try imageData.write(to: fileURL)
+                print("Image saved")
             }
             catch{
                 print(error)
             }
         }
     }
-    
-    @objc private func didTapTakePhoto(){
-        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
-    }
-
 }
-
-
-extension ViewController: AVCapturePhotoCaptureDelegate {
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let data = photo.fileDataRepresentation() else {
-            return
-        }
-        let image = UIImage(data: data)
-        
-        func getDocumentsDirectory() -> URL {
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-            return paths[0]
-        }
-        if let image = UIImage(named: "example.jpg"){
-            if let data = image.jpegData(compressionQuality: 0.8){
-                let filename = getDocumentsDirectory().appendingPathComponent("copy.png")
-                try? data.write(to: filename)
-            }
-        }
-        session?.stopRunning()
-        
-        // Display the captured photo
-        
-        
-        
-        // Keep the boxView visible
-        
-
-        
-        // Optionally, you can save the captured image to your photo library
-        // UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-    }
-    func uploadImage(imagepath: String){
-        let image = UIImage(named: imagepath)
-        let imageData = image?.jpegData(compressionQuality: 1)
-        let fileURL = URL(fileURLWithPath: imagepath)
-        let fileName = fileURL.lastPathComponent
-        let fileContent = imageData?.base64EncodedString()
-        let postData = fileContent!.data(using: .utf8)
-        let APIKey = "NbbVN5cnQoPAAW8NBSNK"
-        
-        // Initialize Inference Server Request with API_KEY, Model, and Model Version
-        var request = URLRequest(url: URL(string: "https://detect.roboflow.com/weed-identification-plxb0/5?api_key=\(APIKey)&name=\(fileName).jpg")!,timeoutInterval: Double.infinity)
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        request.httpBody = postData
-        
-        // Execute Post Request
-        URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
-            
-            // Parse Response to String
-            guard let data = data else {
-                print(String(describing: error))
-                return
-            }
-            
-            // Convert Response String to Dictionary
-            do {
-                let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-            } catch {
-                print(error.localizedDescription)
-            }
-            
-            // Print String Response
-            print(String(data: data, encoding: .utf8)!)
-        }).resume()
-    }
-    
-}
-
-
-
-
-
 
 
